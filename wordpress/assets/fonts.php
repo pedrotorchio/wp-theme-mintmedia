07d656a35fb02929c670f11ecc56817626c6fa69
Pedro Torchio <pedrotorchio@gmail.com>
2019-01-31 13:02:35 -0300

    including custom fonts

---
+++ wordpress/assets/fonts.php
@@ -0,0 +1,6 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Thasadith:400,700', false );
+
+});
--- wordpress/includes.php
+++ wordpress/includes.php
@@ -2,6 +2,7 @@
 $register = [
     THEME_ROOT . '/wordpress/vue/register.php',
     THEME_ROOT . '/wordpress/reset/redirects.php',
+    THEME_ROOT . '/wordpress/assets/fonts.php',
 ];
 foreach($register as $file) {
     require_once($file);
