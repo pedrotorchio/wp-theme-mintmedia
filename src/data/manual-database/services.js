9e8b6003598ba16bf96b11e0b6adf2d931a9ab74
Pedro Torchio <pedrotorchio@gmail.com>
2019-02-01 11:06:39 -0300

    services imgs and video

--- src/data/manual-database/services.js
+++ src/data/manual-database/services.js
@@ -2,44 +2,44 @@ export default {
     cards: [
         { 
             "img": {
-                medium: "#",
-                placeholder: "#"
+                medium: "/wp-content/uploads/2019/02/service-web-e1549027469938-600x300.jpg",
+                placeholder: "/wp-content/uploads/2019/02/service-web-e1549027469938-60x30.jpg"
             },
             "video": {
-                url: "#"
+                url: "/wp-content/uploads/2019/02/service-vid-web.mp4"
             },
             "title" : "Website Development",
             "text": "The digital foundation and window to your unique story. Our developers, designers, and entire team work together to ensure success. "
         },
         { 
             "img": {
-                medium: "#",
-                placeholder: "#"
+                medium: "/wp-content/uploads/2019/02/service-photo-e1549027220499-600x298.jpg",
+                placeholder: "/wp-content/uploads/2019/02/service-photo-e1549027220499-60x30.jpg"
             },
             "video": {
-                url: "#"
+                url: "http://local.mintdesign.media/wp-content/uploads/2019/02/service-vid-marketing.mp4"
             },
             "title" : "Video Marketing",
             "text": "Marketers who use video grow revenue 49% faster than non-video users."
         },
         { 
             "img": {
-                medium: "#",
-                placeholder: "#"
+                medium: "/wp-content/uploads/2019/02/service-drone-e1549027146247-600x300.jpg",
+                placeholder: "/wp-content/uploads/2019/02/service-drone-e1549027146247-60x30.jpg"
             },
             "video": {
-                url: "#"
+                url: "/wp-content/uploads/2019/02/service-vid-drone.mp4"
             },
             "title" : "Drone Images",
             "text": "Stand out with breath taking video and pictures."
         },
         { 
             "img": {
-                medium: "#",
-                placeholder: "#"
+                medium: "/wp-content/uploads/2019/02/service-marketing-e1549028355328-600x300.jpg",
+                placeholder: "/wp-content/uploads/2019/02/service-marketing-e1549028355328-60x30.jpg"
             },
             "video": {
-                url: "#"
+                url: "/wp-content/uploads/2019/02/service-vid-marketing.mp4"
             },
             "title" : "Digital Marketing",
             "text": "We are digital storytellers helping your business or project stand out."
